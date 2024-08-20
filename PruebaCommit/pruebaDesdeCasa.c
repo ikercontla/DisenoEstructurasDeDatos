292d01306f2ba5831adf985cfc55631120359b41
Iker Contla <iker.contla@iteso.mx>
2024-08-20 16:40:14 -0600

    Prueba desde Casa

---
+++ PruebaCommit/pruebaDesdeCasa.c
@@ -0,0 +1,16 @@
+/*
+ * pruebaDesdeCasa.c
+ *
+ *  Created on: Aug 20, 2024
+ *      Author: ikerb
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	printf("Hola Mundo");
+
+	return 0;
+}
