8d623cdd45bfdc6a0e30a23ddf095d47ee5b4bda
Iker Contla <iker.contla@iteso.mx>
2024-10-15 20:51:56 -0600

    hola

---
+++ Proyecto01/PacketsProyect.h
@@ -0,0 +1,17 @@
+/*
+ * PacketsProyect.h
+ *
+ *  Created on: Oct 6, 2024
+ *      Author: ikerb
+ */
+
+#ifndef PACKETSPROYECT_H_
+#define PACKETSPROYECT_H_
+
+class PacketsProyect {
+public:
+	PacketsProyect();
+	virtual ~PacketsProyect();
+};
+
+#endif /* PACKETSPROYECT_H_ */
